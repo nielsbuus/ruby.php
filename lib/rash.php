02f45c3791d793e7daeea5bd52941d4af70892c6
Niels Buus <nielsbuus@gmail.com>
2014-04-27 23:25:04 +0200

    Implements Rash stub

--- lib/rash.php
+++ lib/rash.php
@@ -1,5 +1,30 @@
 <?php
 
-class Rash {
+class Rash implements arrayaccess {
 
+  private $internal_array;
+
+  public function __construct($dictionary) {
+    $this->internal_array = $dictionary;
+  }
+
+  public function offsetExists($offset)
+  {
+    return array_key_exists($offset, $this->internal_array);
+  }
+
+  public function offsetGet($offset)
+  {
+    return $this->internal_array[$offset];
+  }
+
+  public function offsetSet($offset, $value)
+  {
+    $this->internal_array[$offset] = $value;
+  }
+
+  public function offsetUnset($offset)
+  {
+    unset($this->internal_array[$offset]);
+  }
 }
---
+++ tests/RashTest.php
@@ -0,0 +1,10 @@
+<?php
+
+class RashTest extends PHPUnit_Framework_TestCase {
+
+  protected function setUp() {
+
+  }
+
+}
+
