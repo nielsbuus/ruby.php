548d45ba9bf60a697ae5519f897e74c5fb65fa51
Niels Buus <nielsbuus@gmail.com>
2014-05-08 23:15:06 +0200

    Allow factoring of empty rays and rashes

--- lib/functions.php
+++ lib/functions.php
@@ -1,9 +1,15 @@
 <?php
 
 function ray($array) {
+  if(is_null($array)) {
+    $assoc_array = [];
+  }
   return new Ray($array);
 };
 
-function rash($assoc_array) {
+function rash($assoc_array = NULL) {
+  if(is_null($assoc_array)) {
+    $assoc_array = [];
+  }
   return new Rash($assoc_array);
 }
