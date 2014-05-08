f4c8124157109bd4536d1ec7d97a526b6ddd0f0b
Niels Buus <nielsbuus@gmail.com>
2014-05-08 19:28:09 +0200

    Preliminary Rash implementation

--- lib/rash.php
+++ lib/rash.php
@@ -8,6 +8,67 @@ class Rash implements arrayaccess {
     $this->internal_array = $dictionary;
   }
 
+  public function clear() {
+    $this->internal_array = [];
+    return $this;
+  }
+
+  public function delete($key) {
+    $val = $this->internal_array[$key];
+    unset($this->internal_array[$key]);
+    return $val;
+  }
+
+  public function delete_if($handler) {
+    $kept_elements = [];
+    foreach ($this->internal_array as $key => $value) {
+      if($handler($key, $value)) {
+        # do nothing
+      } else {
+        $kept_elements[$key] = $value;
+      }
+    }
+    $this->internal_array = $kept_elements;
+  }
+
+  public function each($handler) {
+    foreach ($this->internal_array as $key => $value) {
+      $handler($key, $value);
+    }
+  }
+
+  public function fetch($key, $default) {
+    # coming
+  }
+
+  public function flatten() {
+    # coming
+  }
+
+  public function has_key($key) {
+    return $this->offsetExists($key);
+  }
+
+  public function has_value($value) {
+    # coming
+  }
+
+  public function invert() {
+    # coming
+  }
+
+  public function keep_if() {
+    # coming
+  }
+
+  public function key($value) {
+    # coming
+  }
+
+  public function keys() {
+    return array_keys($this->internal_array);
+  }
+
   public function offsetExists($offset)
   {
     return array_key_exists($offset, $this->internal_array);
--- tests/RashTest.php
+++ tests/RashTest.php
@@ -2,9 +2,45 @@
 
 class RashTest extends PHPUnit_Framework_TestCase {
 
-  protected function setUp() {
-
+  public function test_clear() {
+    $rash = rash(["name" => "Jack", "age" => 34]);
+    $empty_rash = rash([]);
+    $rash->clear();
+    $this->assertEquals($rash, $empty_rash);
   }
+
+  public function test_clear_return_value() {
+    $rash = rash(["name" => "Jack", "age" => 34]);
+    $empty_rash = rash([]);
+    $this->assertEquals($rash->clear(), $empty_rash);
+  }
+
+  public function test_delete() {
+    $rash = rash(["red" => "FF0000", "green" => "00FF00", "blue" => "0000FF"]);
+    $modified_rash = rash(["red" => "FF0000", "blue" => "0000FF"]);
+    $rash->delete("green");
+    $this->assertEquals($rash, $modified_rash);
+  }
+
+  public function test_delete_return_value() {
+    $rash = rash(["red" => "FF0000", "green" => "00FF00", "blue" => "0000FF"]);
+    $return_value = $rash->delete("green");
+    $this->assertEquals($return_value, "00FF00");
+  }
+
+  public function test_delete_if() {
+    $rash = rash(["red" => "FF0000", "green" => "00FF00", "blue" => "0000FF"]);
+    $modified_rash = rash(["green" => "00FF00", "blue" => "0000FF"]);
+    $rash->delete_if(function($key, $value) {
+      if (strpos($key, 'ed') !== false) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+    $this->assertEquals($modified_rash, $rash);
+  }
+
 
 }
 
